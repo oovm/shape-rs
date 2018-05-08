26dce9c818a8ab2ca6315cadacf18f42f2129573
Aster <192608617@qq.com>
2018-05-08 21:03:33 +0800

    💮 Add Trapezohedron logo

--- projects/delaunay/src/lib.rs
+++ projects/delaunay/src/lib.rs
@@ -1,5 +1,5 @@
 #![deny(rustdoc::missing_crate_level_docs)]
-#![deny(missing_docs)]
+// #![deny(missing_docs)]
 #![deny(missing_debug_implementations)]
 #![doc = include_str!("../Readme.md")]
 #![doc(html_logo_url = "https://avatars.githubusercontent.com/u/11549616")]
