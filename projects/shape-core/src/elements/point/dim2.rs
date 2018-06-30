ab493e8b7b7033e46569256cfeacc5387930ff83
Aster <192608617@qq.com>
2018-07-01 01:07:19 +0800

    🔮 Define `Vector` type

--- projects/shape-core/src/elements/point/dim2.rs
+++ projects/shape-core/src/elements/point/dim2.rs
@@ -7,6 +7,27 @@ impl<T> Point<T> {
     }
 }
 
+impl<T> Point<T>
+where
+    T: Num + Clone,
+{
+    pub fn norm(&self) -> T
+    where
+        T: Float,
+    {
+        (self.x.clone() * self.x.clone() + self.y.clone() * self.y.clone()).sqrt()
+    }
+
+    /// ```math
+    /// \vec{PA}\times\vec{PB} = (a_x-b_x)*(p_y-b_y)-(p_x-b_x)*(a_y-b_y)
+    /// ```
+    pub fn cross_dot(&self, a: &Self, b: &Self) -> T {
+        let p = (b.x.clone() - a.x.clone()) * (self.y.clone() - b.y.clone());
+        let q = (b.y.clone() - a.y.clone()) * (self.x.clone() - b.x.clone());
+        p - q
+    }
+}
+
 // impl Point<T> {
 //     /// Distance between two points.
 //     pub fn distance_to(&self, other: &Self) -> f32 {
--- projects/shape-core/src/elements/rectangle_like/rect.rs
+++ projects/shape-core/src/elements/rectangle_like/rect.rs
@@ -1,1 +1,20 @@
 use super::*;
+
+impl<T> Rectangle<T>
+where
+    T: Clone + Num,
+{
+    pub fn new<P>(anchor: P, side: (T, T)) -> Self
+    where
+        P: Into<Point<T>>,
+    {
+        Self { anchor: anchor.into(), side }
+    }
+    pub fn from_center(anchor: Point<T>, side: (T, T)) -> Self {
+        todo!()
+    }
+    pub fn from_diagonal_points(p1: Point<T>, p2: Point<T>) -> Rectangle<T> {
+        let size = p2.clone() - p1.clone();
+        Self { anchor: p1, side: (size.x, size.y) }
+    }
+}
