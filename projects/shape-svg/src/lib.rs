15442c5b0324aabd8433dc60cb4de87d2a22fe5d
Aster <192608617@qq.com>
2018-07-24 03:53:03 +0800

    🔖 Add documents for svg

--- projects/shape-svg/src/lib.rs
+++ projects/shape-svg/src/lib.rs
@@ -1,3 +1,9 @@
+#![deny(missing_docs)]
+#![deny(missing_debug_implementations)]
+#![doc = include_str!("../Readme.md")]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
+#![doc(html_favicon_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
+
 mod element;
 mod renderer_2d;
 
