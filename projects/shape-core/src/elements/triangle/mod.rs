40784a93cea35e4fef4039fc444fa68dc3b3ae34
Aster <192608617@qq.com>
2018-08-24 06:25:30 +0800

    🔮 Support calculate Triangle's area

--- projects/shape-core/src/elements/triangle/mod.rs
+++ projects/shape-core/src/elements/triangle/mod.rs
@@ -14,8 +14,12 @@ impl<T> Triangle<T> {
         let [a, b, c] = vertex;
         Self { vertex: [a.into(), b.into(), c.into()] }
     }
-    pub fn is_empty(&self) -> bool {
-        true
+}
+
+impl<T: Clone> Triangle<T> {
+    pub fn is_valid(&self) -> bool {
+        let (ab, ac, _) = self.edges();
+        ab == ac
     }
     pub fn is_congruent(&self) -> bool {
         true
@@ -23,8 +27,19 @@ impl<T> Triangle<T> {
     pub fn is_isosceles(&self) -> bool {
         true
     }
-    pub fn area(&self) -> T {
-        // Det[{{x1, y1, 1}, {x2, y2, 1}, {x3, y3, 1}}]/2
+    #[inline]
+    fn edges(&self) -> (Vector<T>, Vector<T>, Vector<T>) {
         todo!()
+    }
+    /// Returns the area of the triangle.
+    pub fn area(&self) -> T {
+        // Det[{{x0, y0, 1}, {x1, y1, 1}, {x2, y2, 1}}] / 2
+        // x0 y1 - x1 y0
+        let det1 = self.vertex[0].x.clone() * self.vertex[1].y.clone() - self.vertex[1].x.clone() * self.vertex[0].y.clone();
+        // x1 y2 - x2 y1
+        let det2 = self.vertex[1].x.clone() * self.vertex[2].y.clone() - self.vertex[2].x.clone() * self.vertex[1].y.clone();
+        // x2 y0 - x0 y2
+        let det3 = self.vertex[2].x.clone() * self.vertex[0].y.clone() - self.vertex[0].x.clone() * self.vertex[2].y.clone();
+        (det1 + det2 + det3) / two()
     }
 }
