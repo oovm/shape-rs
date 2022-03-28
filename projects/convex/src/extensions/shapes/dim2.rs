ebcbae41263c7d3f80684329c8d2746e4d4c3e99
Aster <galaster@foxmail.com>
2022-03-28 20:30:36 +0800

    🔮 Convex Hull Algorithm

--- projects/convex/src/extensions/shapes/dim2.rs
+++ projects/convex/src/extensions/shapes/dim2.rs
@@ -3,10 +3,6 @@ use super::*;
 impl<T> ConvexHull<T> for Point<T> {
     type Output = Polygon<T>;
 
-    fn is_convex_hull(&self, _: Option<T>) -> bool {
-        false
-    }
-
     fn get_convex_hull(&self, _: Option<T>) -> Option<Self::Output> {
         None
     }
@@ -18,36 +14,7 @@ where
 {
     type Output = Polygon<T>;
 
-    fn is_convex_hull(&self, tolerance: Option<T>) -> bool {
-        // Handling special cases
-        match self {
-            [] | [_] | [_, _] => return false,
-            [a, b, c] => {}
-            _ => {}
-        }
-        // Use divide and conquer algorithms for large-scale problems
-        const PARALLEL_IS_FASTER: usize = 131072;
-        match self.len() >= PARALLEL_IS_FASTER {
-            true => {}
-            false => {}
-        }
-        todo!()
-    }
-
     fn get_convex_hull(&self, tolerance: Option<T>) -> Option<Self::Output> {
-        match self {
-            [] | [_] | [_, _] => None,
-            [a, b, c] => match self.is_convex_hull(tolerance) {
-                true => Some(Polygon::new(vec![a, b, c])),
-                false => None,
-            },
-            _ => {
-                todo!()
-            }
-        }
-    }
-
-    fn merge_convex_hulls(lhs: Self::Output, other: &Self::Output) {
         todo!()
     }
 }
