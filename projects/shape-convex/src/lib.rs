46f0019e90223cbf7055e2cff884db3b997a8469
Aster <galaster@foxmail.com>
2022-03-28 21:13:10 +0800

    🎋 Regroup dependence tree

---
+++ projects/shape-convex/src/lib.rs
@@ -0,0 +1,15 @@
+#![deny(rustdoc::missing_crate_level_docs)]
+#![deny(missing_docs)]
+#![deny(missing_debug_implementations)]
+#![doc = include_str!("../Readme.md")]
+#![doc(html_logo_url = "https://avatars.githubusercontent.com/u/11549616")]
+#![doc(html_favicon_url = "https://avatars.githubusercontent.com/u/11549616")]
+
+pub use crate::traits::*;
+
+mod extensions;
+mod traits;
+mod utils;
+
+#[cfg(feature = "shape")]
+pub use graphics_shape as shape;
