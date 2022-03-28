9baebd8fe645d3ddbf2694c8f59b72337a2a1aad
Aster <galaster@foxmail.com>
2022-03-28 16:31:42 +0800

    🔮 Add convex support

---
+++ projects/convex/src/lib.rs
@@ -0,0 +1,10 @@
+#![deny(rustdoc::missing_crate_level_docs)]
+#![deny(missing_docs)]
+#![deny(missing_debug_implementations)]
+#![doc = include_str!("../Readme.md")]
+#![doc(html_logo_url = "https://avatars.githubusercontent.com/u/11549616")]
+#![doc(html_favicon_url = "https://avatars.githubusercontent.com/u/11549616")]
+
+pub use crate::traits::*;
+
+mod traits;
