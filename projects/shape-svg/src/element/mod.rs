a0dc9c587922d2ebc0fd37897c9f12998b7a13da
Aster <192608617@qq.com>
2022-04-01 17:24:11 +0800

    🔖 Add documents for svg

--- projects/shape-svg/src/element/mod.rs
+++ projects/shape-svg/src/element/mod.rs
@@ -1,23 +1,50 @@
 mod display;
-use std::collections::BTreeMap;
+use std::{
+    collections::{btree_map, BTreeMap},
+    slice,
+};
 
+/// The representation of a svg element tree.
+#[derive(Debug, Clone, PartialEq)]
 pub struct SVG {
+    /// The element type of svg node
     element: &'static str,
+    /// The attributes of svg node
     attributes: BTreeMap<&'static str, String>,
+    /// The children of svg node
     children: Vec<SVG>,
 }
 
 impl SVG {
+    /// Create a new svg element with given name and attributes.
     pub fn new(element: &'static str, attributes: Vec<(&'static str, String)>, children: Vec<SVG>) -> Self {
         Self { element, attributes: BTreeMap::from_iter(attributes.into_iter()), children }
     }
+    /// Insert new attribute to current svg node.
     pub fn insert_attribute<S>(&mut self, keys: &'static str, value: S)
     where
         S: Into<String>,
     {
         self.attributes.insert(keys, value.into());
     }
+    /// View all attributes of current svg node.
+    pub fn attributes(&self) -> btree_map::Iter<'_, &'static str, String> {
+        self.attributes.iter()
+    }
+    /// Get mutable reference of all attributes
+    pub fn attributes_mut(&mut self) -> btree_map::IterMut<'_, &'static str, String> {
+        self.attributes.iter_mut()
+    }
+    /// Insert new child to current svg node.
     pub fn insert_child(&mut self, child: SVG) {
         self.children.push(child);
+    }
+    /// View all children of current svg node.
+    pub fn children(&self) -> slice::Iter<'_, SVG> {
+        self.children.iter()
+    }
+    /// Get mutable reference of all children
+    pub fn children_mut(&mut self) -> slice::IterMut<'_, SVG> {
+        self.children.iter_mut()
     }
 }
