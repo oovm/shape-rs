52171499ee5585d489ee614cdb617a80d093e1dd
Aster <192608617@qq.com>
2022-04-24 19:13:03 +0800

    🔧 Fix `cargo test`

--- projects/shape-core/src/traits/random/mod.rs
+++ projects/shape-core/src/traits/random/mod.rs
@@ -1,13 +1,13 @@
 use crate::{Circle, Point};
-use rand::{thread_rng, Rng};
+use num_traits::{real::Real, FloatConst};
+use rand::{distributions::uniform::SampleUniform, Rng};
 
 impl<T> Circle<T>
 where
-    T: Clone,
+    T: Clone + Real + FloatConst + SampleUniform,
 {
     pub fn random_point(&self, mut rng: impl Rng) -> Point<T> {
-        let x = rng.gen_range(self.x - self.radius.clone(), self.x + self.radius.clone());
-        let y = rng.gen_range(self.y - self.radius.clone(), self.y + self.radius.clone());
-        Point::new(x, y)
+        let _ = rng.gen_range(T::zero()..(T::PI() * T::from(2.0).unwrap()));
+        todo!()
     }
 }
